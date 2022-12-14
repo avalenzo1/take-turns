e7e11679a3bde26224fc1ffbd25d3b97cebcf5ff
Glitch (glitch-hello-website) <none>
2022-12-14 13:44:03 +0000

    💗🚍 Checkpoint ./components.js:45080849/1575 ./index.html:45080849/88 ./script.js:45080849/30

--- components.js
+++ components.js
@@ -1,12 +1,24 @@
 class ViewController {
-  constructor({ id }) {
+  constructor({ id, default: defaultView }) {
     this.id = id;
+    this.defaultView = defaultView;
     
     this.initController();
   }
   
   initController() {
     this.viewController = document.getElementById(this.id);
+    this.viewList = this.viewController.querySelectorAll("[data-view]");
+  }
+  
+  setCurrentView(currentView) {
+    for (let view of this.viewList) {
+      view.removeAttribute("data-current-view");
+    }
+    
+    this.currentView = this.viewList.querySelect(`[data-view='${currentView}']`);
+    this.currentView.setAttribute("data-current-view", "");
+    
   }
 }
 
--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 import { ViewController } from './components.js';
 
-let viewController = new ViewController({ id: "view-controller" });
+let viewController = new ViewController({ id: "view-controller", default: "home-view" });
