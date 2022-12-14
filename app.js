c3a30a0a6e2eaf6747b93c8bd8f7d9bdfccb8772
Glitch (glitch-hello-website) <none>
2022-12-14 21:02:54 +0000

    🚄🏄 Checkpoint ./app.js:45080849/1723 ./package.json:45080849/873 ./public/index.html:45080849/517

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  
+});
+
+server.listen(port, () => {
+  console.log("listening on *:" + port);
+});
