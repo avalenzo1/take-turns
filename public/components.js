6d73484c8bf906a1e0e794f9f9de28f1e143ff60
Glitch (glitch-hello-website) <none>
2022-12-14 21:14:52 +0000

    📅🎥 Checkpoint ./public/index.js:45080849/1122 ./public/style.css:45080849/637 ./public/components.js:45080849/1707 ./public/index.html:45080849/115

--- public/components.js
+++ public/components.js
@@ -8,15 +8,33 @@ class ViewController {
   
   initController() {
     this.viewController = document.getElementById(this.id);
-    this.viewList = this.viewController.querySelectorAll("[data-view]");
+    this.setEvents();
+    
+    if (this.defaultView) {
+      this.setCurrentView(this.defaultView);
+    }
+  }
+  
+  setEvents() {
+    this.viewLinks = this.viewController.querySelectorAll("[data-link]");
+    
+    for (let link of this.viewLinks) {
+      let currentView = link.getAttribute("data-link");
+      
+      link.onclick = () => {
+        this.setCurrentView(currentView);
+      };
+    }
   }
   
   setCurrentView(currentView) {
+    this.viewList = this.viewController.querySelectorAll("[data-view]");
+    
     for (let view of this.viewList) {
       view.removeAttribute("data-current-view");
     }
     
-    this.currentView = this.viewController.querySelect(`[data-view='${currentView}']`);
+    this.currentView = this.viewController.querySelector(`[data-view='${currentView}']`);
     this.currentView.setAttribute("data-current-view", "");
   }
 }
