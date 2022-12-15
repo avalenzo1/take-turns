d61024d7ccdaa3e33f6d94d3945dccb95cb090b0
Glitch (glitch-hello-website) <none>
2022-12-15 22:00:08 +0000

    🍘📹 Checkpoint ./public/index.js:45080849/49195

--- public/index.js
+++ public/index.js
@@ -5,77 +5,53 @@ const socket = io();
 const game = new Game();
 const controller = new ViewController({
   id: "view-controller",
-  default: "homeView",
+  default: "home-view",
 });
 
 controller.mount({
-  homeView: {
+  "home-view": {
     mounted(view) {},
+
+    unmounted(view) {},
   },
 
-  newView: {
+  "new-view": {
     mounted(view) {
       socket.emit("server/new-room");
-
       new Snackbar({
         id: "snackbar-container",
         message: "Creating Room",
         type: "success",
       });
     },
+
+    unmounted(view) {},
   },
 
-  joinView: {
-    mounted(view) {
-      let input = document.getElementById("join-view/room-input");
-      let submit = document.getElementById("join-view/room-submit");
-
-      submit.addEventListener("click", (e) => {
+  "join-view": {
+    elements: {
+      submit: document.getElementById("join-view/room-submit"),
+    },
+    events: {
+      submit(e) {
         e.preventDefault();
-
-        socket.emit("server/join-room", input.value);
-      });
+        socket.emit(
+          "server/join-room",
+          document.getElementById("join-view/room-input").value
+        );
+      },
+    },
+    mounted(view) {
+      this.elements.submit.addEventListener("click", this.events.submit);
+    },
+    unmounted(view) {
+      this.elements.submit.removeEventListener("click", this.events.submit);
     },
   },
 
-  lobbyView: {
+  "lobby-view": {
     events: {
       details(details) {
-        let input = document.getElementById("new-view/room-url");
-        let share = document.getElementById("new-view/room-share");
-        let list = document.getElementById("new-view/player-list");
-        let counter = document.getElementById("new-view/player-count");
-        let ready = document.getElementById("new-view/player-ready");
-
-        let shareData = {
-          title: "Take Turns",
-          text: "Play Take Turns with Me!",
-          url: "https://take-turns.glitch.me/?join=",
-        };
-
-        share.addEventListener("click", async () => {
-          await navigator.share(shareData);
-        });
-
-        input.addEventListener("click", () => {
-          input.select();
-          input.setSelectionRange(0, 99999); // For mobile devices
-
-          // Copy the text inside the text field
-          navigator.clipboard.writeText(input.value);
-
-          new Snackbar({
-            id: "snackbar-container",
-            message: "Copied Link",
-            type: "success",
-          });
-        });
-
-        ready.addEventListener("click", () => {
-          socket.emit("server/player-ready", game.uid);
-          ready.setAttribute("disabled", "");
-        });
-
         shareData.url = input.value =
           "https://take-turns.glitch.me/?join=" + details.uid;
         list.innerHTML = "";
@@ -91,14 +67,40 @@ controller.mount({
         }
 
         game.uid = details.uid;
-      },
+      }
     },
-    mounted(view) {
-      socket.on("server/room-details", this.events.details);
-    },
+    mounted() {
+      let ready = document.getElementById("new-view/player-ready");
 
-    unmounted(view) {
-      socket.off("server/room-details", this.events.details);
+      let shareData = {
+        title: "Take Turns",
+        text: "Play Take Turns with Me!",
+        url: "https://take-turns.glitch.me/?join=",
+      };
+
+      share.addEventListener("click", async () => {
+        await navigator.share(shareData);
+      });
+
+      input.addEventListener("click", () => {
+        input.select();
+        input.setSelectionRange(0, 99999); // For mobile devices
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(input.value);
+
+        new Snackbar({
+          id: "snackbar-container",
+          message: "Copied Link",
+          type: "success",
+        });
+      });
+
+      ready.addEventListener("click", () => {
+        socket.emit("server/player-ready", game.uid);
+      });
+
+      socket.once("server/room-details", this.events.details);
     },
   },
 });
@@ -113,6 +115,16 @@ controller.mount({
     socket.emit("server/join-room", roomUID);
   }
 })();
+
+socket.on("server/new-room", function (roomUID) {
+  new Snackbar({
+    id: "snackbar-container",
+    message: "Room Created",
+    type: "success",
+  });
+
+  socket.emit("server/join-room", roomUID);
+});
 
 socket.on("server/join-room", function (res) {
   switch (res.type) {
@@ -131,8 +143,6 @@ socket.on("server/join-room", function (res) {
         type: "danger",
       });
   }
-  
-  
 });
 
 // client-side
